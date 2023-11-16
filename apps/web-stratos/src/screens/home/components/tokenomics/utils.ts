31edd9081fa348264bede7ac8d20374ee18ab69c
BoThe1K <bohdan@thestratos.org>
2023-11-16 17:55:01 +0100

    feat/qb-2301: Add apis for coingecko/marketcap

--- apps/web-stratos/src/screens/home/components/tokenomics/utils.ts
+++ apps/web-stratos/src/screens/home/components/tokenomics/utils.ts
@@ -1,6 +1,11 @@
-import { BigNumberish } from 'ethers';
+import { BigNumberish, FixedNumber } from 'ethers';
 import { formatUnits } from 'ethers/lib/utils';
 
 export function prettyFormat(value: BigNumberish, unitName?: number, points = 8): number {
   return +(+formatUnits(value, unitName)).toFixed(points);
 }
+
+export const bigToNativeDecimals = (bn: string, decimals?: number): string =>
+  FixedNumber.fromString(bn)
+    .divUnsafe(FixedNumber.from((10 ** (decimals || 18)).toString()))
+    .toString();
